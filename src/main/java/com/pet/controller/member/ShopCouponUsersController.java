08070bcbeefe5de1b5673269f0bb31d15d40e412
cc503280-wq <cc503280@gmail.com>
2026-01-21 08:55:45 +0800

    update coupon user

---
+++ src/main/java/com/pet/controller/member/ShopCouponUsersController.java
@@ -0,0 +1,34 @@
+package com.pet.controller.member;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pet.model.member.CouponUsers;
+import com.pet.service.member.CouponUsersService;
+import com.pet.util.LoginUser;
+
+@RestController
+@RequestMapping("/shop/coupons")
+public class ShopCouponUsersController {
+
+    @Autowired
+    private CouponUsersService couponUsersService; 
+
+    //取得目前登入者的優惠券清單
+    @GetMapping
+    public ResponseEntity<?> getMyCoupons(@LoginUser Integer userId) {
+        
+        if (userId == null) {
+            return ResponseEntity.status(401).body("未登入");
+        }
+
+        List<CouponUsers> myCoupons = couponUsersService.getCouponUsersByMemberId(userId);
+
+        return ResponseEntity.ok(myCoupons);
+    }
+}
