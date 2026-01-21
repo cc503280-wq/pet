c38880013fd95f61d505f74011b9eea95f18d5f4
Richy <richytravelc@gmail.com>
2026-01-21 14:41:59 +0800

    update

--- src/main/java/com/pet/model/member/CouponUsersReal.java
+++ src/main/java/com/pet/model/member/CouponUsersReal.java
@@ -2,9 +2,9 @@ package com.pet.model.member;
 
 import java.time.LocalDate;
 
-
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,20 +16,31 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-@Entity @Table(name = "couponUsers")
+@Entity
+@Table(name = "coupon_users")
 @Component
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class CouponUsersReal {
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@Column(name = "member_id")
 	private Integer memberId;
+
+	@Column(name = "coupon_id")
 	private Integer couponId;
+
 	@NonNull
 	private String status;
+
+	@Column(name = "assigned_at")
 	private LocalDate assignedAt;
+
 	@NonNull
+	@Column(name = "used_at")
 	private LocalDate usedAt;
 }
