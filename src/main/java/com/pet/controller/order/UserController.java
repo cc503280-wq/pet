694cbca0c9718863033cc05de9afe86e37489bdb
zine <wow99500@gmail.com>
2026-01-24 11:04:30 +0800

    前端控制器更新

--- src/main/java/com/pet/controller/order/UserController.java
+++ src/main/java/com/pet/controller/order/UserController.java
@@ -4,17 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.pet.controller.member.AdminAuthController;
+import com.pet.dto.order.OrderItemDTO;
 import com.pet.model.order.Order;
 import com.pet.model.order.OrderItem;
+import com.pet.model.order.Shipment;
 import com.pet.service.order.OrderItemService;
 import com.pet.service.order.OrderService;
+import com.pet.service.order.ShipmentService;
 import com.pet.service.product.ProductService;
 import com.pet.util.LoginUser;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/shop/userOrder")
@@ -29,6 +35,9 @@ public class UserController {
 	
 	@Autowired
 	private ProductService pService;
+	
+	@Autowired
+	private ShipmentService shipmentService;
 
     UserController(AdminAuthController adminAuthController) {
         this.adminAuthController = adminAuthController;
@@ -52,7 +61,14 @@ public class UserController {
 	
 	@GetMapping("/detail")
 	@ResponseBody
-	public List<OrderItem> userOrderItems(Integer orderId){
-		return null;
+	public List<OrderItemDTO> userOrderItems(Integer orderId){
+		return oiService.getOrderItemDTO(orderId);
 	}
+	
+	@GetMapping("/shipFee")
+	public Shipment getMethodName(Integer orderId) {
+		return shipmentService.getShipmentByOrderId(orderId).get(0);
+	}
+	
+	
 }
