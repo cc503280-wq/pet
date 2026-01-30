111f7e8bc79fa1ac01e11a4a59d08ccc4caea633
RCYJ <86285973+RichyChenYJ@users.noreply.github.com>
2026-01-30 12:41:06 +0800

    統整共用資訊

--- src/main/java/com/pet/common/AppConstants.java
+++ src/main/java/com/pet/common/AppConstants.java
@@ -10,6 +10,7 @@ public class AppConstants {
     public static final String APPOINTMENT_STATUS_CANCELLED = "已取消";
     public static final String APPOINTMENT_STATUS_COMPLETED = "已完成";
     public static final String APPOINTMENT_STATUS_IN_PROGRESS = "進行中";
+    public static final String APPOINTMENT_STATUS_CHECKED_IN = "已報到";
     public static final String APPOINTMENT_STATUS_NO_SHOW = "未到店";
     
     // 時段相關
@@ -19,6 +20,18 @@ public class AppConstants {
     public static final String DEFAULT_START_TIME = "09:00";
     public static final String DEFAULT_END_TIME = "21:00";
     
+    // WebSocket Topic
+    public static final String TOPIC_ADMIN_APPOINTMENTS = "/topic/admin/appointments";
+    public static final String TOPIC_MEMBER_PREFIX = "/topic/member/%d/appointments";
+    public static final String TOPIC_GROOMER_PREFIX = "/topic/groomer/%d/appointments";
+
+    // WebSocket Event Type
+    public static final String EVENT_NEW_APPOINTMENT = "NEW_APPOINTMENT";
+    public static final String EVENT_CANCELLED = "APPOINTMENT_CANCELLED";
+    public static final String EVENT_COMPLETED = "APPOINTMENT_COMPLETED";
+    public static final String EVENT_CHECKIN = "APPOINTMENT_CHECKIN";
+    public static final String EVENT_START = "APPOINTMENT_START";
+
     private AppConstants() {
         // 防止實例化
     }
