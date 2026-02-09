21f26cd05fc720d2363c2849af7b48f18b01c00c
RCYJ <86285973+RichyChenYJ@users.noreply.github.com>
2026-02-09 15:30:30 +0800

    將AuditDashboard.html改為Modal模式，置入Groomer頁面。刪除AuditDashboard.html，刪除PerformanceMonitorAspect，

--- src/main/resources/static/dist/js/admin-layout.js
+++ src/main/resources/static/dist/js/admin-layout.js
@@ -82,7 +82,6 @@ const sidebarHTML = `
                 <li class="nav-item">
                     <a href="#" class="nav-link"><i class="nav-icon fas fa-edit"></i><p>人員管理 <i class="right fas fa-angle-left"></i></p></a>
                     <ul class="nav nav-treeview">
-					    <li class="nav-item"><a href="AuditDashboard.html" class="nav-link"><i class="far fa-circle nav-icon"></i><p>系統監控儀表板</p></a></li>
                         <li class="nav-item"><a href="GetAllGroomers.html" class="nav-link"><i class="far fa-circle nav-icon"></i><p>美容師列表</p></a></li>
                         <li class="nav-item"><a href="GetAllLeaveRecords.html" class="nav-link"><i class="far fa-circle nav-icon"></i><p>休假審核</p></a></li>
                     </ul>
